22271d843d8ba16aa24bb94f83a342e65ca1ca09
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-27 11:23:04 -0700

    ..

--- lib/array_list.rb
+++ lib/array_list.rb
@@ -2,15 +2,21 @@
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0] #native array: fixed size, indexing only
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    raise "Bad things happen" if @size == 10
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    raise "Bad things happen" if @size == 0
+    @size -= 1
   end
 
   def include?(key)
@@ -22,6 +28,24 @@ class ArrayList
   def max
   end
 
-  def to_s
+  def to_s #toString
+    str = ""
+    @size.times do |i|
+     str += "#{@storage[i]}, "
+    end
+    return "[#{str[0...-3]}]"
   end
+  #
+  # def removeAll(list)
+  #   arry = []
+  #   @size.times do |i|
+  #     if list[i] != 3
+  #       arry << string(i)
+  #     end
+  #   end
+  #   return arry
+  # end
+
 end
+
+  
